ce5b5c4c5824cd1aba51b7d2b57fcbccf0281b7b
Clay.Tran <clay.tran@avepoint.com>
2024-07-19 21:40:48 +0700

    "Translated Vietnamese labels in product modal component and added new labels to vi.json file"

--- src/pages/category/c-pages/product/components/Modal.jsx
+++ src/pages/category/c-pages/product/components/Modal.jsx
@@ -2,47 +2,50 @@ import { Button } from '@/components/buttons/buttons';
 import { AddUser } from '@/container/pages/style';
 import { BasicFormWrapper } from '@/container/styled';
 import { AutoComplete, Form, Input } from 'antd';
+import { useTranslation } from 'react-i18next';
 
 const ModalHangHoa = ({ form, handleOk, state, onCancel, loading, textSubmit }) => {
+  const { t } = useTranslation();
+
   return (
     <AddUser>
       <BasicFormWrapper>
         <Form form={form} name="contactEdit" onFinish={handleOk}>
           <Form.Item
             initialValue={state?.update.mahang}
-            label="Mã hàng"
+            label={t('Product_Code')}
             name="mahang"
             required
-            rules={[{ message: 'Vui lòng nhập mã hàng' }]}
+            rules={[{ message: t('Product_RequiredCode') }]}
           >
             <Input />
           </Form.Item>
 
-          <Form.Item label="Đơn vị tính" name="donViTinh" initialValue={state?.update.donViTinh}>
+          <Form.Item label={t('Product_Unit')} name="donViTinh" initialValue={state?.update.donViTinh}>
             <Input />
           </Form.Item>
 
-          <Form.Item initialValue={state?.update.taiKhoanHang} name="taiKhoanHang" label="Tài khoản hàng hoá (15_)">
+          <Form.Item initialValue={state?.update.taiKhoanHang} name="taiKhoanHang" label={t('Product_AccountGoods')}>
             <Input />
           </Form.Item>
 
-          <Form.Item initialValue={state?.update.taiKhoanGiaVon} name="taiKhoanGiaVon" label="Tài khoản giá vốn (63_)">
+          <Form.Item initialValue={state?.update.taiKhoanGiaVon} name="taiKhoanGiaVon" label={t('Product_AccountCost')}>
             <Input />
           </Form.Item>
 
           <Form.Item
             initialValue={state?.update.taiKhoanDoanhThu}
             name="taiKhoanDoanhThu"
-            label="Tài khoản doanh thu (51_)"
+            label={t('Product_AccountRevenue')}
           >
             <Input />
           </Form.Item>
 
-          <Form.Item initialValue={state?.update.tenHangBan} name="tenHangBan" label="Tên hàng bán ra">
+          <Form.Item initialValue={state?.update.tenHangBan} name="tenHangBan" label={t('Product_SellingName')}>
             <AutoComplete />
           </Form.Item>
 
-          <Form.Item initialValue={state?.update.tenHangMua} name="tenHangMua" label="Tên hàng mua vào">
+          <Form.Item initialValue={state?.update.tenHangMua} name="tenHangMua" label={t('Product_PurchasingName')}>
             <Input />
           </Form.Item>
 
@@ -56,7 +59,7 @@ const ModalHangHoa = ({ form, handleOk, state, onCancel, loading, textSubmit }) 
               }}
               onClick={onCancel}
             >
-              Huỷ bỏ
+              {t('Common_Cancel')}
             </Button>
             <Button htmlType="submit" size="default" type="primary" key="submit" loading={loading}>
               {textSubmit}
