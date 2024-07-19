43e981e84d8b36e4e70f224044f2a2e648d4c2e7
Clay.Tran <clay.tran@avepoint.com>
2024-07-19 23:07:12 +0700

    "Removed React import from dropdown.js, updated header-right/index.js with new imports and state management, and modified flag images and language options."

--- src/layout/header-right/index.js
+++ src/layout/header-right/index.js
@@ -6,30 +6,32 @@ import UilSignout from '@iconscout/react-unicons/icons/uil-signout';
 import UilUser from '@iconscout/react-unicons/icons/uil-user';
 import UilUsersAlt from '@iconscout/react-unicons/icons/uil-users-alt';
 import { Avatar } from 'antd';
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 
-import { logOut } from '../../redux/authentication/actionCreator';
-import Message from './Message';
-import Notification from './Notification';
-import { InfoWrapper, UserDropDown } from './Style';
+import { InfoWrapper, NavAuth, UserDropDown } from './Style';
 
+import { logOut } from '@/redux/authentication/actionCreator';
+import ChatAuthImg from '@/static/img/avatar/chat-auth.png';
+import EngImg from '@/static/img/flag/en.png';
+import VieImg from '@/static/img/flag/vi.png';
+import { useTranslation } from 'react-i18next';
 import Heading from '../../components/heading/heading';
 import { Popover } from '../../components/popup/popup';
 
 const AuthInfo = React.memo(() => {
   const dispatch = useDispatch();
-
-  // const [state, setState] = useState({
-  //   flag: 'en',
-  // });
   const navigate = useNavigate();
-  // const { i18n } = useTranslation();
+  const { i18n } = useTranslation();
+
+  const [state, setState] = useState({
+    flag: 'vi',
+  });
+  const { flag } = state;
 
   const SignOut = (e) => {
     e.preventDefault();
-
     dispatch(logOut(() => navigate('/')));
   };
 
@@ -37,10 +39,9 @@ const AuthInfo = React.memo(() => {
     <UserDropDown>
       <div className="user-dropdown">
         <figure className="user-dropdown__info">
-          <img src={require('../../static/img/avatar/chat-auth.png')} alt="" />
+          <img src={ChatAuthImg} alt="" />
           <figcaption>
             <Heading as="h5">Cristiano Ronaldo</Heading>
-            <p>UI Expert</p>
           </figcaption>
         </figure>
         <ul className="user-dropdown__links">
@@ -77,45 +78,41 @@ const AuthInfo = React.memo(() => {
     </UserDropDown>
   );
 
-  // const onFlagChangeHandle = (value, e) => {
-  //   e.preventDefault();
-  //   setState({
-  //     ...state,
-  //     flag: value,
-  //   });
-  //   i18n.changeLanguage(value);
-  // };
+  const onFlagChangeHandle = (value, e) => {
+    e.preventDefault();
+    setState({
+      ...state,
+      flag: value,
+    });
+    i18n.changeLanguage(value);
+  };
 
-  // const country = (
-  //   <NavAuth>
-  //     <Link onClick={(e) => onFlagChangeHandle('en', e)} to="#">
-  //       <img src={require('../../static/img/flag/en.png')} alt="" />
-  //       <span>English</span>
-  //     </Link>
-  //     <Link onClick={(e) => onFlagChangeHandle('esp', e)} to="#">
-  //       <img src={require('../../static/img/flag/esp.png')} alt="" />
-  //       <span>Spanish</span>
-  //     </Link>
-  //     <Link onClick={(e) => onFlagChangeHandle('ar', e)} to="#">
-  //       <img src={require('../../static/img/flag/ar.png')} alt="" />
-  //       <span>Arabic</span>
-  //     </Link>
-  //   </NavAuth>
-  // );
+  const country = (
+    <NavAuth>
+      <Link onClick={(e) => onFlagChangeHandle('vi', e)} to="#">
+        <img width="20" src={VieImg} alt="" />
+        <span>Vietnamese</span>
+      </Link>
+      <Link onClick={(e) => onFlagChangeHandle('en', e)} to="#">
+        <img src={EngImg} alt="" />
+        <span>English</span>
+      </Link>
+    </NavAuth>
+  );
 
   return (
     <InfoWrapper>
       {/* <Search /> */}
-      <Message />
-      <Notification />
+      {/* <Message /> */}
+      {/* <Notification /> */}
       {/* <Settings /> */}
-      {/* <div className="invoice-nav-actions__item invoice-nav-actions__language">
-        <Dropdown placement="bottomRight" content={country} trigger="click">
+      <div className="invoice-nav-actions__item invoice-nav-actions__language">
+        <Popover placement="bottomRight" content={country} trigger="click">
           <Link to="#" className="invoice-nav-action-link">
-            <img src={require(`../../../static/img/flag/${flag}.png`)} alt="" />
+            <img width="25" src={require(`@/static/img/flag/${flag}.png`)} alt="" />
           </Link>
-        </Dropdown>
-      </div> */}
+        </Popover>
+      </div>
       <div className="invoice-nav-actions__item invoice-nav-actions__author">
         <Popover placement="bottomRight" content={userContent} action="click">
           <Link to="#" className="invoice-nav-action-link">
