286ad400f22c612ae34ef3846a285e8fde624d6e
Clay.Tran <clay.tran@avepoint.com>
2024-07-21 20:44:47 +0700

    "Deleted multiple Redux action creators, actions, and reducers files"

--- src/redux/rootReducers.js
+++ src/redux/rootReducers.js
@@ -2,24 +2,16 @@ import { combineReducers } from 'redux';
 import authReducer from './authentication/reducers';
 import dataTable from './data-filter/reducers';
 import galleryReducer from './gallary/reducers';
-import { headerSearchReducer } from './headerSearch/reducers';
-import jobs from './jobs/reducers';
-import Note from './note/reducers';
-import { readNotificationReducer } from './notification/reducers';
 import Profile from './profile/reducers';
 import ChangeLayoutMode from './themeLayout/reducers';
 import themeUsersReducer from './themeUsers/reducers';
 
 const rootReducers = combineReducers({
   themeUsers: themeUsersReducer,
-  headerSearchData: headerSearchReducer,
-  notification: readNotificationReducer,
   auth: authReducer,
   gallery: galleryReducer,
   ChangeLayoutMode,
-  Note,
   Profile,
-  jobs,
   dataTable,
 });
 
