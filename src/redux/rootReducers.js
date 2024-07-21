57c17044686d7a917c8b0b5e51ed1284bd9f0ded
Clay.Tran <clay.tran@avepoint.com>
2024-07-21 20:45:27 +0700

    "Removed gallery related Redux files and references"

--- src/redux/rootReducers.js
+++ src/redux/rootReducers.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux';
 import authReducer from './authentication/reducers';
 import dataTable from './data-filter/reducers';
-import galleryReducer from './gallary/reducers';
 import Profile from './profile/reducers';
 import ChangeLayoutMode from './themeLayout/reducers';
 import themeUsersReducer from './themeUsers/reducers';
@@ -9,7 +8,6 @@ import themeUsersReducer from './themeUsers/reducers';
 const rootReducers = combineReducers({
   themeUsers: themeUsersReducer,
   auth: authReducer,
-  gallery: galleryReducer,
   ChangeLayoutMode,
   Profile,
   dataTable,
