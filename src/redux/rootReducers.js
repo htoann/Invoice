bca893bd0183193da09d03f0903820018d05d074
Clay.Tran <clay.tran@avepoint.com>
2024-07-21 20:45:45 +0700

    "Deleted profile related Redux files (actionCreator, actions, reducers) and removed Profile reducer from rootReducers."

--- src/redux/rootReducers.js
+++ src/redux/rootReducers.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux';
 import authReducer from './authentication/reducers';
 import dataTable from './data-filter/reducers';
-import Profile from './profile/reducers';
 import ChangeLayoutMode from './themeLayout/reducers';
 import themeUsersReducer from './themeUsers/reducers';
 
@@ -9,7 +8,6 @@ const rootReducers = combineReducers({
   themeUsers: themeUsersReducer,
   auth: authReducer,
   ChangeLayoutMode,
-  Profile,
   dataTable,
 });
 
