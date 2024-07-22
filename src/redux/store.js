4a933f68ce4ac1055d53a5837f60284c61f3992f
Clay.Tran <clay.tran@avepoint.com>
2024-07-22 13:53:04 +0700

    "Removed redux-thunk dependency and its usage in store.js"

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,6 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
+import { createStore } from 'redux';
 import rootReducer from './rootReducers';
 
-const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
+const store = createStore(rootReducer);
 
 export default store;
