be00b4f07d8663dda3d37e6a4d88e2e06b87b264
Clay.Tran <clay.tran@avepoint.com>
2024-07-22 10:54:15 +0700

    "Removed themeUsers reducer and related files, renamed ChangeLayoutMode to changeLayoutMode in rootReducers"

--- src/redux/rootReducers.js
+++ src/redux/rootReducers.js
@@ -1,13 +1,11 @@
 import { combineReducers } from 'redux';
 import authReducer from './authentication/reducers';
 import dataTable from './data-filter/reducers';
-import ChangeLayoutMode from './themeLayout/reducers';
-import themeUsersReducer from './themeUsers/reducers';
+import changeLayoutMode from './themeLayout/reducers';
 
 const rootReducers = combineReducers({
-  themeUsers: themeUsersReducer,
   auth: authReducer,
-  ChangeLayoutMode,
+  changeLayoutMode,
   dataTable,
 });
 
