b180983579ecd74b68736f88228c4c3c6273ca98
Clay.Tran <clay.tran@avepoint.com>
2024-07-30 17:38:27 +0700

    feat: PURGE DAY

--- src/components/datePicker/datePicker.js
+++ src/components/datePicker/datePicker.js
@@ -1,64 +1,7 @@
 import { DatePicker } from 'antd';
-import React, { useState } from 'react';
-import { DateRangePicker } from 'react-date-range';
-import 'react-date-range/dist/styles.css'; // main style file
-import 'react-date-range/dist/theme/default.css'; // theme css file
-import { ButtonGroup, ItemWrapper } from './style';
-import { Button } from '../buttons/buttons';
-
-function DateRangePickerOne() {
-  const [state, setState] = useState({
-    datePickerInternational: null,
-    dateRangePicker: {
-      selection: {
-        startDate: new Date(),
-        endDate: new Date(),
-        key: 'selection',
-      },
-    },
-  });
-
-  const handleRangeChange = (which) => {
-    setState({
-      ...state,
-      dateRangePicker: {
-        ...state.dateRangePicker,
-        ...which,
-      },
-    });
-  };
-
-  const { dateRangePicker } = state;
-  const start = dateRangePicker.selection.startDate.toString().split(' ');
-  const end = dateRangePicker.selection.endDate.toString().split(' ');
-
-  return (
-    <ItemWrapper>
-      <DateRangePicker
-        onChange={handleRangeChange}
-        showSelectionPreview
-        moveRangeOnFirstSelection={false}
-        className="PreviewArea"
-        months={2}
-        ranges={[dateRangePicker.selection]}
-        direction="horizontal"
-      />
-
-      <ButtonGroup>
-        <p>{`${start[1]} ${start[2]} ${start[3]} - ${end[1]} ${end[2]} ${end[3]}`}</p>
-        <Button size="small" type="primary">
-          Apply
-        </Button>
-        <Button size="small" type="white" outlined>
-          Cancel
-        </Button>
-      </ButtonGroup>
-    </ItemWrapper>
-  );
-}
+import React from 'react';
 
 class CustomDateRange extends React.Component {
-  // eslint-disable-next-line react/state-in-constructor
   state = {
     startValue: null,
     endValue: null,
@@ -137,4 +80,4 @@ class CustomDateRange extends React.Component {
   }
 }
 
-export { DateRangePickerOne, CustomDateRange };
+export { CustomDateRange };
