70d4356633a471ffb2c63ce937fba708b9eb6895
Clay.Tran <clay.tran@avepoint.com>
2024-08-12 15:57:23 +0700

    feat: ellipsis name orgs

--- src/pages/category/c-pages/organization/components/MenuItem.jsx
+++ src/pages/category/c-pages/organization/components/MenuItem.jsx
@@ -3,7 +3,17 @@ import EditButton from './EditButton';
 
 const MenuItem = ({ item, onEdit, onDelete, loading }) => (
   <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-    {item.name}
+    <div
+      style={{
+        overflow: 'hidden',
+        textOverflow: 'ellipsis',
+        whiteSpace: 'nowrap',
+        alignItems: 'center',
+        width: '100%',
+      }}
+    >
+      {item.name}
+    </div>
     <div style={{ marginRight: 8, display: 'flex', alignItems: 'center' }}>
       <EditButton onClick={onEdit} />
       <DeleteButton onConfirm={onDelete} loading={loading} />
