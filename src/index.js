f39930da51f2501b96b0e9a51e982c873b52e96c
Toan Tran <huutrantoan@gmail.com>
2024-09-04 20:35:04 +0700

    Removed React.StrictMode, reordered imports, and updated API endpoint in apiConst.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import i18n from './i18n/config';
 
 import { App } from 'App';
-import { AuthProvider } from 'context/AuthContext';
 import { AppProvider } from 'context/AppContext';
+import { AuthProvider } from 'context/AuthContext';
 import dayjs from './utils/dayjs';
 
 const locale = i18n.language;
@@ -16,11 +15,9 @@ const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
-  <React.StrictMode>
-    <AuthProvider>
-      <AppProvider>
-        <App />
-      </AppProvider>
-    </AuthProvider>
-  </React.StrictMode>,
+  <AuthProvider>
+    <AppProvider>
+      <App />
+    </AppProvider>
+  </AuthProvider>,
 );
