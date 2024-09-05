feaca000c1ceb21eb3fcdbe63fb0c4ffb17d0fd9
Clay.Tran <clay.tran@avepoint.com>
2024-09-05 16:45:44 +0700

    Refactor Organization component to use useOrgStructure hook, removing individual useGetBranches, useGetDepartments, and useGetProjects hooks

--- src/pages/category/c-pages/organization/Organization.jsx
+++ src/pages/category/c-pages/organization/Organization.jsx
@@ -1,10 +1,7 @@
 import { PageHeader } from '@/components/page-headers/page-headers';
 import { Main } from '@/container/styled';
 import { Row } from 'antd';
-import { useGetBranches } from 'hooks/org-structure/useGetBranches';
-import { useGetDepartments } from 'hooks/org-structure/useGetDepartments';
-import { useGetProjects } from 'hooks/org-structure/useGetProjects';
-import { useState } from 'react';
+import { useOrgStructure } from 'hooks/org-structure';
 import { useTranslation } from 'react-i18next';
 import BranchList from './c-pages/Branch';
 import DepartmentList from './c-pages/Department';
@@ -13,22 +10,26 @@ import ProjectList from './c-pages/Project';
 export const Organization = () => {
   const { t } = useTranslation();
 
-  const { branches, setBranches, loadingBranches, getBranches } = useGetBranches();
-  const [selectedBranchId, setSelectedBranchId] = useState(null);
-
   const {
-    selectedDepartmentId,
+    branches,
+    setBranches,
+    getBranches,
+    loadingBranches,
+    selectedBranchId,
+    setSelectedBranchId,
+
     departments,
     setDepartments,
     getDepartments,
     loadingDepartments,
-    setSelectedDepartmentId,
-  } = useGetDepartments(selectedBranchId);
-
-  const { projects, setProjects, getProjects, loadingProjects } = useGetProjects(
-    selectedBranchId,
     selectedDepartmentId,
-  );
+    setSelectedDepartmentId,
+
+    projects,
+    setProjects,
+    getProjects,
+    loadingProjects,
+  } = useOrgStructure();
 
   const onResetDeleteBranch = () => {
     setSelectedBranchId(null);
