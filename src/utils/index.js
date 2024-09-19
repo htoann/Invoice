62f773586cc763ae16f5b44cb758cc0931dece08
Clay.Tran <clay.tran@avepoint.com>
2024-09-19 23:49:06 +0700

    feat: i18n date picker

--- src/utils/index.js
+++ src/utils/index.js
@@ -176,9 +176,11 @@ export const customLocale = {
   DatePicker: {
     lang: {
       ...getAntdLocale().DatePicker?.lang,
-      shortWeekDays: ['CN', 'T2', 'T3', 'T4', 'T5', 'T6', 'T7'],
-      shortMonths: ['Th1', 'Th2', 'Th3', 'Th4', 'Th5', 'Th6', 'Th7', 'Th8', 'Th9', 'Th10', 'Th11', 'Th12'],
       firstDayOfWeek: 1,
+      ...(i18n.language === 'vi' && {
+        shortWeekDays: ['CN', 'T2', 'T3', 'T4', 'T5', 'T6', 'T7'],
+        shortMonths: ['Th1', 'Th2', 'Th3', 'Th4', 'Th5', 'Th6', 'Th7', 'Th8', 'Th9', 'Th10', 'Th11', 'Th12'],
+      }),
     },
   },
 };
