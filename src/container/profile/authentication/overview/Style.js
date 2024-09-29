24a771384c9239097da2c9a83cc13bd83752316b
Clay.Tran <clay.tran@avepoint.com>
2024-09-29 22:35:14 +0700

    "Update padding value in AuthenticationWrap from 120px to 100px"

--- src/container/profile/authentication/overview/Style.js
+++ src/container/profile/authentication/overview/Style.js
@@ -2,7 +2,7 @@ import Styled from 'styled-components';
 
 const AuthenticationWrap = Styled.div`
   position: relative;
-  padding: 120px 0;
+  padding: 100px 0;
   background-position: top;
   background-repeat: no-repeat;
   background-size: 100%;
